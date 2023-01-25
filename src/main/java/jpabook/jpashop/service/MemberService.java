0c099a656dba3d3e7e8e68faa69366a67ea7824e
sukmuzi <sukmuzi@naver.com>
2023-01-25 15:38:32 +0900

    회원 테스트 코드 작성

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,57 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    // 1. 필드 @Autowired
+    // 2. 생성자에 초기화
+    // 3. @RequiredArgsConstructor 사용 (추천)
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     */
+    @Transactional // readOnly false
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+    /**
+     * 중복 회원 검증
+     */
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 회원 전체 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 단건 조회
+     */
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
