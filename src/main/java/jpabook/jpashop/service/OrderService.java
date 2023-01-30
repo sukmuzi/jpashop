27b9897f2069e6d5363323fa5d10b7f24b6dedf8
sukmuzi <sukmuzi@naver.com>
2023-01-30 16:15:27 +0900

    상품 주문 목록 취소 추가

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -8,6 +8,7 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -37,20 +38,18 @@ public class OrderService {
         delivery.setAddress(member.getAddress());
 
         /*
+          주문상품 생성, 주문 생성
           생성 메서드를 사용하지 않고 객체 생성해서 set 하는 경우를 방지하기 위해 entity 클래스에 @NoArgsConstructor access level 을 protected 로 설정함.
 
           서비스 계층은 단순히 엔티티에 필요한 요청을 위임하는 역할을 한다.
           이처럼 엔티티가 비즈니스 로직을 자기고 객체 지향의 특성을 적극 활용하는 것을 도메인 모델 패턴이라 한다.
           반대로 서비스 계층에서 대부분의 비즈니스 로직을 처리하는 것을 트랜잭션 스크립트 패턴이라 한다.
          */
-        // 주문상품 생성
         OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
-
-        // 주문 생성
         Order order = Order.createOrder(member, delivery, orderItem);
 
-        // 주문 저장
         /*
+          주문 저장
           cascade 가 전부 걸려있기 때문에 order 만 save 함
           지금은 주문, 상품, 배송이 같이 묶여있기 때문에 이렇게 하는거고 다른 곳에서도 참조하는 일이 생긴다하면
           cascade 옵션을 고려하고 별도의 repository 를 만들어서 persist 처리해야함.
@@ -71,11 +70,10 @@ public class OrderService {
         order.cancel();
     }
 
-
     /**
      * 검색
      */
-//    public List<Order> findOrders(OrderSearch orderSearch) {
-//        return orderRepository.findAll(orderSearch);
-//    }
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAllByJpql(orderSearch);
+    }
 }
